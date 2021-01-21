4c0790586b1eb5360df0d8be25c7ebc01cd487fc
njrapidinnovation <68365436+njrapidinnovation@users.noreply.github.com>
2021-01-21 17:21:32 +0530

    search api added contract abi code change in gameblock file

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -70,12 +70,12 @@ export class NotificationService {
         await this.Eventgateway.wss.to(id).emit('notification',"notification sent successfully")
     }
     async findUser(username:string):Promise<any>{
-        const result = await this.user.find({ username: { $regex: `((?i)${username})`} ||  { $regex: `/^${username}/i`} })
-        console.log(result);
-        let usernames = [];
-        for(let i=0;i<result.length;i++)
-        usernames.push(result[i].username);
-        return usernames;
+        const result = await this.user.find({ username: { $regex: `((?i)${username})`} ||  { $regex: `/^${username}/i`} },{username:1,publickey:1})
+        // console.log(result);
+        // let usernames = [];
+        // for(let i=0;i<result.length;i++)
+        // usernames.push(result[i].username.concat);
+        return result;
       }
 
 }
